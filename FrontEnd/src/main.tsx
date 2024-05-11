45ac36e07fc83ca2fcb089d515e20f573af68f4f
ThakoorRishwanth <rishijay13@gmail.com>
2024-05-11 13:36:25 +0530

    data

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -8,7 +8,6 @@ import { store } from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-
   <BrowserRouter>
     <App />
   </BrowserRouter>
